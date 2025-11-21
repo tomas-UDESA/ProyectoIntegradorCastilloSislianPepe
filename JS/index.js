9d92390eee2d464d3d99834e6bda88a8f995b661
tomas-UDESA <tcastilloramos@udesa.edu.ar>
2025-11-21 10:48:32 -0300

    (hecho por maximo sislian y se quedo sin bateria) js del index hecho, y html id agregado

--- JS/index.js
+++ JS/index.js
@@ -1,26 +1,10 @@
-let formulario = document.querySelector(".searchbarform");
-let campoBusqueda = document.querySelector(".searchbar");
+let primeraSeccion = document.getElementById("productos");
+let segundaSeccion = document.getElementById("masVendidos");
 
-if (formulario && campoBusqueda) {
+if (primeraSeccion) {
+    primeraSeccion.innerHTML = "";
 
-    formulario.onsubmit = function (evento) {
-
-        let texto = campoBusqueda.value.trim();
-
-        if (texto === "") {
-            evento.preventDefault();
-            alert("El campo de búsqueda no puede estar vacío.");
-        }
-
-        else if (texto.length < 3) {
-            evento.preventDefault();
-            alert("El término buscado debe tener al menos 3 caracteres.");
-        }
-
-    };
-}
-
-    fetch("https://dummyjson.com/products/category/smartphones")
+    fetch("https://dummyjson.com/products/category/smartphones?limit=10")
         .then(function (res) {
             return res.json();
         })
@@ -28,27 +12,29 @@ if (formulario && campoBusqueda) {
             let productos = data.products;
 
             for (let i = 0; i < productos.length && i < 10; i++) {
-
                 let p = productos[i];
 
                 let tarjeta =
-                    `<div class="divcate"> 
-                        <img src="${p.thumbnail}" alt="'  p.title  '" class="produim">
-                        <p class="produtit">${p.title}  </p>
-                        <p class="produdes">${p.description} </p>
-                        <p class="produprec">${p.price} </p> 
-                        <a href"./product.html?id=${p.id}" class="produinfo">Ver detalle.</a> 
+                    `<div class="divcate">
+                        <img src="${p.thumbnail}" class="produim">
+                        <p class="produtit">${p.title}</p>
+                        <p class="produdes">${p.description}</p>
+                        <p class="produprec">$${p.price}</p>
+                        <a href="./product.html?id=${p.id}" class="produinfo">Ver detalle.</a>
                     </div>`;
 
                 primeraSeccion.innerHTML += tarjeta;
             }
         })
         .catch(function (error) {
-            console.log("Error en primera sección:", error);
+            console.log("Error en smartphones:", error);
         });
+}
 
+if (segundaSeccion) {
+    segundaSeccion.innerHTML = "";
 
-    fetch("https://dummyjson.com/products/category/groceries")
+    fetch("https://dummyjson.com/products/category/groceries?limit=10")
         .then(function (res) {
             return res.json();
         })
@@ -56,21 +42,21 @@ if (formulario && campoBusqueda) {
             let productos = data.products;
 
             for (let i = 0; i < productos.length && i < 10; i++) {
-
                 let p = productos[i];
 
                 let tarjeta =
-                    `<div class="divcate">' +
-                        <img src="${p.thumbnail}" alt="' ${p.title}  '" class="produim"> 
+                    `<div class="divcate">
+                        <img src="${p.thumbnail}" class="produim">
                         <p class="produtit">${p.title}</p>
                         <p class="produdes">${p.description}</p>
-                        <p class="produprec">${p.price}</p> 
-                        <a href="./product.html?id=${producto.id}" class="produinfo">Ver detalle.</a>
+                        <p class="produprec">$${p.price}</p>
+                        <a href="./product.html?id=${p.id}" class="produinfo">Ver detalle.</a>
                     </div>`;
 
-segundaSeccion.innerHTML += tarjeta;
+                segundaSeccion.innerHTML += tarjeta;
             }
         })
         .catch(function (error) {
-            console.log("Error en segunda sección:", error);
+            console.log("Error en groceries:", error);
         });
+}
